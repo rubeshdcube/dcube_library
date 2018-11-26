5b55ab209a047cfeb8a86db2ff487969351cd3b2
rubeshdcube <rubesh@d3analytics.com>
2018-11-26 09:29:12 +0530

    new

---
+++ dcube-0.1/dcube-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='dcube',
+	  version='0.1',
+	  description='Library for Symphony Data Model',
+	  author='Dcube Analytics Pvt Ltd',
+	  author_email='dds@d3analytics.com',
+	  Packages=['dcube', 'dcube.symphony'],
+	  )
+
+__author__ = 'Dcube Analytics Pvt Ltd'
+
